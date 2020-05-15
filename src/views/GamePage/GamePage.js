286dab73235cf465710b4d3591ff2075a81d53e2
Nora Fu <noorafuu@gmail.com>
2020-05-14 22:53:50 -0700

    btn text change

--- src/views/GamePage/GamePage.js
+++ src/views/GamePage/GamePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 // @material-ui/core components
+import classes from "./GamePage.module.css";
 import { makeStyles } from "@material-ui/core/styles";
 import HeaderSearchBar from "components/HeaderSearchBar/HeaderSearchBar.js";
 import { TransitionGroup } from "react-transition-group";
@@ -8,25 +9,25 @@ import stylesLogin from "assets/jss/material-kit-react/views/singerPage.js";
 import Button from "@material-ui/core/Button";
 // import styles from "assets/jss/material-kit-react/views/components.js";
 
-const styles = makeStyles((theme) => ({
-  stylesLogin,
-  buttonDark: {
-    paddingLeft: "50px",
-    paddingRight: "50px",
-    borderRadius: "35px",
-    backgroundColor: "#3D1B7C",
-    borderColor: "#3D1B7C",
-    color: "#FFFFFF",
-    fontSize: 20,
-    fontFamily: ["Noto Sans", "sans-serif"].join(","),
-    textTransform: "none",
-    margin: "10px",
-    "&:hover": {
-      backgroundColor: "#6B5899",
-      borderColor: "#6B5899",
-    },
-  },
-}));
+// const styles = makeStyles((theme) => ({
+//   stylesLogin,
+//   buttonDark: {
+//     paddingLeft: "50px",
+//     paddingRight: "50px",
+//     borderRadius: "35px",
+//     backgroundColor: "#3D1B7C",
+//     borderColor: "#3D1B7C",
+//     color: "#FFFFFF",
+//     fontSize: 20,
+//     fontFamily: ["Noto Sans", "sans-serif"].join(","),
+//     textTransform: "none",
+//     margin: "10px",
+//     "&:hover": {
+//       backgroundColor: "#6B5899",
+//       borderColor: "#6B5899",
+//     },
+//   },
+// }));
 
 class GamePage extends Component {
   state = {
@@ -39,11 +40,12 @@ class GamePage extends Component {
   // let btnText = "Next";
   transitionHandler = () => {
     if (this.state.next == false) {
-      // state.next = true;
-      // btnText = "submit";
+      this.setState({
+        next: true,
+        btnText: "submit",
+      });
     } else if (this.state.next == true && this.state.submit == false) {
-      // state.submit = true;
-      // btnText = "submit";
+      this.setState({ submit: true });
     }
   };
   // const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
@@ -58,9 +60,13 @@ class GamePage extends Component {
       <div>
         <HeaderSearchBar />
 
-        <Button varient="contained" onClick={this.transitionHandler}>
+        <button
+          varient="contained"
+          onClick={this.transitionHandler}
+          className={classes.buttonDark}
+        >
           {this.state.btnText}
-        </Button>
+        </button>
       </div>
     );
   }
