1b3b4cf91134cc7d35f5fab9858c086352103f71
Nora Fu <noorafuu@gmail.com>
2020-05-14 22:37:43 -0700

    gamepage

--- src/views/GamePage/GamePage.js
+++ src/views/GamePage/GamePage.js
@@ -1,65 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 // @material-ui/core components
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import HeaderSearchBar from "components/HeaderSearchBar/HeaderSearchBar.js";
-import { TransitionGroup } from 'react-transition-group';
+import { TransitionGroup } from "react-transition-group";
 
 import stylesLogin from "assets/jss/material-kit-react/views/singerPage.js";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 // import styles from "assets/jss/material-kit-react/views/components.js";
 
 const styles = makeStyles((theme) => ({
   stylesLogin,
   buttonDark: {
-    paddingLeft:"50px",
-    paddingRight:"50px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
     borderRadius: "35px",
-    backgroundColor: '#3D1B7C',
-    borderColor: '#3D1B7C',
-    color: '#FFFFFF',
+    backgroundColor: "#3D1B7C",
+    borderColor: "#3D1B7C",
+    color: "#FFFFFF",
     fontSize: 20,
-    fontFamily: ['Noto Sans', 'sans-serif'].join(','),
-    textTransform: 'none',
-    margin: '10px',
-    '&:hover': {
-      backgroundColor: '#6B5899',
-      borderColor: '#6B5899'
-    }
-  }
+    fontFamily: ["Noto Sans", "sans-serif"].join(","),
+    textTransform: "none",
+    margin: "10px",
+    "&:hover": {
+      backgroundColor: "#6B5899",
+      borderColor: "#6B5899",
+    },
+  },
 }));
 
-
-
-export default function GamePage(props) {
-  const state = {
-    singerChosen = false,
-    next = false,
-    lyricsChosen = false,
-    submit = false
+class GamePage extends Component {
+  state = {
+    singerChosen: false,
+    next: false,
+    lyricsChosen: false,
+    submit: false,
+    btnText: "Next",
   };
+  // let btnText = "Next";
   transitionHandler = () => {
+    if (this.state.next == false) {
+      // state.next = true;
+      // btnText = "submit";
+    } else if (this.state.next == true && this.state.submit == false) {
+      // state.submit = true;
+      // btnText = "submit";
+    }
+  };
+  // const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  // setTimeout(function() {
+  //   setCardAnimation("");
+  // }, 700);
+  // const classes = styles();
+  // const { ...rest } = props;
+  render() {
+    // const classes = styles();
+    return (
+      <div>
+        <HeaderSearchBar />
 
-  }
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function() {
-    setCardAnimation("");
-  }, 700);
-  const classes = styles();
-  const {
-    ...rest
-  } = props;
-  return (
-    <div>
-    <HeaderSearchBar/>
-    <TransitionGroup
-          transitionName="background"
-          transitionEnterTimeout={1000}
-          transitionLeaveTimeout={1000}
-        >
-        <Button varient="contained" className={classes.buttonDark} onClick={}>
-        Next
+        <Button varient="contained" onClick={this.transitionHandler}>
+          {this.state.btnText}
         </Button>
-    </TransitionGroup>
-    </div>);
-
+      </div>
+    );
+  }
 }
+
+export default GamePage;
