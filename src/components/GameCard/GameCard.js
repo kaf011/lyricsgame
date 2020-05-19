c9e6d1c0aab0e62e1b2c47b988cf0e8ed2e92936
cleoxiao <yax064@ucsd.edu>
2020-05-18 19:26:40 -0700

    gamecard

---
+++ src/components/GameCard/GameCard.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import ts_profile from "assets/img/taylor-profile.jpg";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody";
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+let fontTheme = createMuiTheme();
+fontTheme = responsiveFontSizes(fontTheme);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "auto",
+    width: "95%",
+  },
+  media: {
+    height: 0,
+    marginTop: "8%",
+    width: "97%",
+    paddingTop: "100%" // 16:9
+  },
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    fontSize: 4,
+  },
+}));
+
+export default function KeyWordCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+
+  return (
+    <Card className={classes.root}>
+      <CardBody>
+        <CardMedia className={classes.media} 
+        component="img"
+        height = "100%"
+        image={ts_profile} />
+        <CardContent>
+          <ThemeProvider theme={fontTheme}>
+            <Typography variant="body1" align="center" width="75%">
+              Taylor Swift
+            </Typography>
+          </ThemeProvider>
+        </CardContent>
+      </CardBody>
+    </Card>
+  );
+}
